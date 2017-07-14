3eb5c7a335ed902f7f6fd1dcd72bc995c4784f39
Hui Jiang <huijiang@Huis-MBP.home>
2017-07-14 12:30:17 -0400

    user show paged updated to render via json and jquery

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,26 @@
 $(document).on('turbolinks:load', function(){
-  	
-  	
+
+	var user_id = parseInt($(".main-user").attr("user-id"));
+
+	//get user info in json format
+	//display meetings and events
+	if (user_id) {//check to see if on user page
+		$.get("/users/" + user_id +".json", function(data){ 
+			meetingData = data["data"]["attributes"]["meetings"];
+			eventData = data["data"]["attributes"]["events"];
+			loadMeetings(meetingData);
+	  		loadEvents(eventData);
+		});	
+	}
 });
+
+function loadMeetings(data){
+	var meetingsHTML = HandlebarsTemplates['events']({events : data});
+	$(".organized-meetings").html(meetingsHTML);
+}
+
+function loadEvents(data){
+	var eventsHTML = HandlebarsTemplates['events']({events : data});
+	$(".participating-events").html(eventsHTML);
+
+}
