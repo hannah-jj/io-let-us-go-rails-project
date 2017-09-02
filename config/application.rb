5e8dbc2df161643ec811a93446ae5342967447ee
Hui Jiang <huijiang@Huis-MBP.home>
2017-09-01 23:45:32 -0400

    updated config file

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module LetUsGo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.time_zone = 'Eastern Time (US & Canada)'
-
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
